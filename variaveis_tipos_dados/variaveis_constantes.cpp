d1f4543d1e1ccec6a08b6f85922e51e5c4786ebe
LabreCoder <jvlabremachado@gmail.com>
2026-02-03 22:41:44 -0300

    Adicionando variáveis constantes

---
+++ variaveis_tipos_dados/variaveis_constantes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int age;
+    age = 25;
+    cout << age << endl;
+    age = 30;
+    cout << age << endl;
+    age = 35;
+    cout << age << endl;
+    
+    const int age2 = 30;
+    cout << age2 << endl;
+
+    return 0;
+}
