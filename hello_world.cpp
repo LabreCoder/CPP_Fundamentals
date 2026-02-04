3d574bdefd5909c42f2b9c946593fdaee52cf224
LabreCoder <jvlabremachado@gmail.com>
2026-02-03 22:14:42 -0300

    Adicionando o primeiro código em C++ !!

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Definição do namespace std
+using namespace std;
+
+// Função principal
+int main(int argc, char const* argv[])
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
